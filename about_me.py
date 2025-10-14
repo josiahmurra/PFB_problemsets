3f4bdd32f6def0436a03b73e633c83072cc9aae8
josiahmurra <josiahmurra@gmail.com>
2025-10-14 11:16:43 -0400

    added a function about my favorite things

--- about_me.py
+++ about_me.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
-my_name = "Josiah"
+# import sys module
+import sys
 
+# asign variable values
+my_name = sys.argv[1] #"Josiah"
+my_color = "Green"
+my_activity = "Night Disc"
+my_animal = "Octopus"
+
+# print output
 print("My name: ", my_name)
+print("My favorite color: ", my_color)
+print("My favorite activity: ", my_activity)
+print(f'My favorite animal: {my_animal}')
+
